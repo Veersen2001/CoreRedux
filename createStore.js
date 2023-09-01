8996e5c79ee6e25246fd4aa6c5d8a2e9ca35a67c
Veerbahadur Sen <veerbahadursen@gmail.com>
2023-09-01 21:51:35 +0530

    Core Redux

--- createStore.js
+++ createStore.js
@@ -4,7 +4,12 @@ function todoReducer(state,action)
 {
     if(action.type == 'add_Todo')
     {
-        
+        const todoText = action.payload.todoText;
+
+        return[
+            ...state,
+            {text:todoText,isFinished: false, id:(state.length == 0)? 1: state[state.length - 1].id + 1}
+        ]
     }
 }
 const response = createStore();
